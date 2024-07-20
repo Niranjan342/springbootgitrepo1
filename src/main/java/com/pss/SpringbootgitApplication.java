d8646f4a69b174487846f226a0329a58fe4f715a
Your Name <you@example.com>
2024-07-20 16:25:58 +0530

    intial commit

---
+++ src/main/java/com/pss/SpringbootgitApplication.java
@@ -0,0 +1,14 @@
+package com.pss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootgitApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
